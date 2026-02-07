7858e705d8d4c45db89448e7a0dcbae6202d4441
ed <null@localhost.local>
2026-02-06 17:39:40 -0700

    Remove HacksPanel, add config-driven rainbow and auto-battle
    Replace interactive HacksPanel UI with ENABLE_RAINBOW and
    ENABLE_AUTO_BATTLE config flags. Rainbow is enabled via
    GameUI.set_pixels_controller monkey-patch. Auto-battle sets
    app.auto_test_enabled at startup. Both default to False and
    are intended to be injected by the host-side flash script.

--- modded_firmware/main.py
+++ modded_firmware/main.py
@@ -37,151 +37,8 @@ ALL_ACHS = [
 
 NVS_NAMESPACES = ['cactuscon', 'write']
 
-
-class HacksPanel:
-    _app_ref = None
-
-    def __init__(self, screen, game_ui, battle_manager, logger_level=None, memory_efficient=False):
-        from cactuscon.utils import Logger
-        self.logger = Logger(logger_level)
-        self.tft = screen
-        self.display = screen.display if screen else None
-        self.game_ui = game_ui
-        self.battle_manager = battle_manager
-        self.memory_efficient = memory_efficient
-        self.screen = None
-        self.ui_elements = {}
-        self.loaded = False
-        self.visible = False
-        self._rainbow_on = False
-        self._auto_battle_on = False
-        self._hue = 0
-        self._color_timer = None
-        self._rainbow_lbl = None
-        self._auto_lbl = None
-
-    def load(self):
-        import lvgl as lv
-        if self.loaded:
-            return
-        self.screen = lv.obj()
-        self.create_ui()
-        self.loaded = True
-
-    def create_ui(self):
-        import lvgl as lv
-        scr = self.screen
-        scr.set_style_bg_color(lv.color_black(), 0)
-        scr.set_style_pad_all(0, 0)
-        scr.remove_flag(lv.obj.FLAG.SCROLLABLE)
-
-        # Title with rainbow color cycling
-        title = lv.label(scr)
-        title.set_text('Hacks')
-        title.set_style_text_color(lv.color_hex(0x00FF00), 0)
-        title.align(lv.ALIGN.TOP_MID, 0, 10)
-        self.ui_elements['title'] = title
-
-        self._hue = 0
-        def _cycle_color(timer):
-            self._hue = (self._hue + 5) % 360
-            c = lv.color_hsv_to_rgb(self._hue, 100, 100)
-            title.set_style_text_color(c, 0)
-        self._color_timer = lv.timer_create(_cycle_color, 50, None)
-
-        # Back button
-        btn_back = lv.button(scr)
-        btn_back.set_size(60, 30)
-        btn_back.align(lv.ALIGN.TOP_RIGHT, -5, 5)
-        btn_back.set_style_bg_color(lv.color_hex(0x1A1A1A), 0)
-        btn_back.set_style_bg_color(lv.color_hex(0x2A2A2A), lv.STATE.PRESSED)
-        btn_back.set_style_border_color(lv.color_hex(0x00FF00), 0)
-        btn_back.set_style_border_width(2, 0)
-        btn_back.set_style_radius(8, 0)
-        lbl_back = lv.label(btn_back)
-        lbl_back.set_text('Back')
-        lbl_back.set_style_text_color(lv.color_hex(0x00FF00), 0)
-        lbl_back.center()
-        btn_back.add_event_cb(self._on_back, lv.EVENT.CLICKED, None)
-
-        # Rainbow toggle
-        btn_rainbow = lv.button(scr)
-        btn_rainbow.set_size(200, 35)
-        btn_rainbow.align(lv.ALIGN.TOP_MID, 0, 65)
-        btn_rainbow.set_style_bg_color(lv.color_hex(0x1A1A1A), 0)
-        btn_rainbow.set_style_bg_color(lv.color_hex(0x2A2A2A), lv.STATE.PRESSED)
-        btn_rainbow.set_style_border_color(lv.color_hex(0x00FF00), 0)
-        btn_rainbow.set_style_border_width(2, 0)
-        btn_rainbow.set_style_radius(8, 0)
-        self._rainbow_lbl = lv.label(btn_rainbow)
-        self._rainbow_lbl.set_text('Rainbow: OFF')
-        self._rainbow_lbl.set_style_text_color(lv.color_hex(0x00FF00), 0)
-        self._rainbow_lbl.center()
-        btn_rainbow.add_event_cb(self._on_rainbow, lv.EVENT.CLICKED, None)
-
-        # Auto-Battle toggle
-        btn_auto = lv.button(scr)
-        btn_auto.set_size(200, 35)
-        btn_auto.align(lv.ALIGN.TOP_MID, 0, 115)
-        btn_auto.set_style_bg_color(lv.color_hex(0x1A1A1A), 0)
-        btn_auto.set_style_bg_color(lv.color_hex(0x2A2A2A), lv.STATE.PRESSED)
-        btn_auto.set_style_border_color(lv.color_hex(0x00FF00), 0)
-        btn_auto.set_style_border_width(2, 0)
-        btn_auto.set_style_radius(8, 0)
-        self._auto_lbl = lv.label(btn_auto)
-        self._auto_lbl.set_text('Auto-Battle: OFF')
-        self._auto_lbl.set_style_text_color(lv.color_hex(0x00FF00), 0)
-        self._auto_lbl.center()
-        btn_auto.add_event_cb(self._on_auto_battle, lv.EVENT.CLICKED, None)
-
-    def _on_back(self, event):
-        self.game_ui.show_panel('main_menu')
-
-    def _on_rainbow(self, event):
-        self._rainbow_on = not self._rainbow_on
-        pixels = getattr(self.game_ui, 'pixels', None)
-        if pixels:
-            if self._rainbow_on:
-                pixels.start_rainbow()
-                self._rainbow_lbl.set_text('Rainbow: ON')
-            else:
-                pixels.off()
-                self._rainbow_lbl.set_text('Rainbow: OFF')
-
-    def _on_auto_battle(self, event):
-        self._auto_battle_on = not self._auto_battle_on
-        app = HacksPanel._app_ref
-        if app:
-            app.auto_test_enabled = self._auto_battle_on
-        if self._auto_battle_on:
-            self._auto_lbl.set_text('Auto-Battle: ON')
-        else:
-            self._auto_lbl.set_text('Auto-Battle: OFF')
-
-    def show(self):
-        import lvgl as lv
-        if self.screen:
-            lv.screen_load(self.screen)
-            self.visible = True
-
-    def on_show(self):
-        pass
-
-    def on_hide(self):
-        if self._color_timer:
-            self._color_timer.delete()
-            self._color_timer = None
-        self.visible = False
-
-    def on_cleanup(self):
-        self.on_hide()
-
-    def unload(self):
-        self.on_hide()
-        if self.screen:
-            self.screen.delete()
-            self.screen = None
-        self.loaded = False
+ENABLE_RAINBOW = False
+ENABLE_AUTO_BATTLE = False
 
 
 def _patch_stats():
@@ -249,38 +106,19 @@ def _patch_stats():
 
 
 def _patch_menu():
-    import lvgl as lv
-    from cactuscon.ui.panels.main_menu import MainMenuPanel
-    _orig_create_ui = MainMenuPanel.create_ui
-
-    def _patched_create_ui(self):
-        _orig_create_ui(self)
-        sub = self.ui_elements.get('subtitle')
-        if sub:
-            sub.set_text('hacked by ed')
-
-        # Reposition Extras button to the left
-        btn_extras = self.ui_elements.get('btn_extras')
-        if btn_extras:
-            btn_extras.align(lv.ALIGN.TOP_MID, -75, 165)
-
-        # Add Hacks button on the right
-        btn_hacks = self._create_menu_button('Hacks', 75, 165)
-        btn_hacks.add_event_cb(self._on_hacks_clicked, lv.EVENT.CLICKED, None)
-        self.ui_elements['btn_hacks'] = btn_hacks
-
-    def _on_hacks_clicked(self, event):
-        if 'hacks' not in self.game_ui.panel_classes:
-            self.game_ui.register_panel('hacks', HacksPanel)
-        self.game_ui.show_panel('hacks')
-
-    MainMenuPanel.create_ui = _patched_create_ui
-    MainMenuPanel._on_hacks_clicked = _on_hacks_clicked
+    from cactuscon.ui.graphics import GameUI
+    _orig_set_pixels = GameUI.set_pixels_controller
+    def _patched_set_pixels(self, pc):
+        _orig_set_pixels(self, pc)
+        if ENABLE_RAINBOW:
+            pc.start_rainbow()
+    GameUI.set_pixels_controller = _patched_set_pixels
 
 
 def main():
     app = BadgeApplication()
-    HacksPanel._app_ref = app
+    if ENABLE_AUTO_BATTLE:
+        app.auto_test_enabled = True
     _patch_stats()
     _patch_menu()
 
